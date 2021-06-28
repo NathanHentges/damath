b1675dda4cf39e36518f6a4a41b1f4e51610c779
Nathan Hentges <nathanjhentges@gmail.com>
2021-06-28 17:48:26 -0500

    initial create-react-app

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <nav className='navbar navbar-expand-lg fixed-bottom navbar-dark bg-primary'>
+      <div className='container'>
+        <p>Copyright</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg mb-5">
+      <div className="container">
+        <a className="navbar-brand" href="/">Damath</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
